60da7ee0ea7f064fd45278d899b0cc0e39e1b190
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-04 07:26:22 +0100

    Mocked model in NotesView test

--- NotesView.js
+++ NotesView.js
@@ -4,16 +4,14 @@ class NotesView {
   constructor(model = new notesModel) {
     this.model = model
     this.mainContainerEl = document.querySelector('#main-container');
-    console.log(this.mainContainerEl);
   }
 
   displayNotes() {
-
-    this.notes = this.model.getNotes();
-    for (let i = 0; i < this.notes.length; i++) {
+    let notes = this.model.getNotes();
+    for (let i = 0; i < notes.length; i++) {
       let newElement = document.createElement('div');
       newElement.classList.add('note')
-      newElement.innerText = `${this.notes[i]}`;
+      newElement.innerText = `${notes[i]}`;
       this.mainContainerEl.append(newElement);
     };
   };
--- NotesView.test.js
+++ NotesView.test.js
@@ -5,25 +5,20 @@
 const fs = require('fs');
 const { hasUncaughtExceptionCaptureCallback } = require('process');
 const NotesView = require('./NotesView');
-const notesModel = require('./notesModel')
 
 const mockedModel = {
-  getNotes: () => {
-    ['This is an example note']
-  }
+  getNotes: () => ['This is an example note', 'Another note']
+  //corrected the syntax - there were curly brackets that shouldn't be there.
+  
 };
 
 describe('NotesView', () => {
   describe('.displayNotes', () => {
     it("gets the notes from model and displays it as a new div element with class 'note'", () => {
       document.body.innerHTML = fs.readFileSync('./index.html');
-      const model = new notesModel
-      model.addNote('This is an example note');
-      const notesView = new NotesView(model);
-
+      const notesView = new NotesView(mockedModel);
       notesView.displayNotes();
-
-      expect(document.querySelectorAll('div.note').length).toBe(1);
+      expect(document.querySelectorAll('div.note').length).toBe(2);
 
     })
   })
